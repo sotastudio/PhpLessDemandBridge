a85849a009065f875de6b277253e865ccb955e77
Andy Hausmann <andy.hausmann@gmx.de>
2011-12-15 18:27:27 +0100

    Revised in order of simplification and cleanup.

--- config.php
+++ config.php
@@ -1,7 +1,40 @@
 <?php
+/**
+ * Demand Bridge to use phpless in a customizable way
+ *
+ * Config file to set program default
+ *
+ * This is required to ensure that this package will work properly; especially some paths need to be predefined.
+ * Supported parsing modes are demand parsing, compiling and both parallel.
+ *
+ * The demand mode provided kinda fetching of really necessary GET vars to customize the rendering.
+ * In this mode there are two important things:
+ * - a cache file for faster return of teh css stuff
+ * - returned headers to control caching and make this hole thing even faster
+ *
+ * The compile mode is actually the base functionality of phpless - of course it has been optimized a bit.
+ *
+ * @package PhpLessDemandBridge
+ * @link https://github.com/MorphexX/PhpLessDemandBridge
+ * @author Andy Hausmann <andy.hausmann@gmx.de>
+ * @copyright 2011 Andy Hausmann <andy.hausmann@gmx.de>
+ * @version 0.1.0
+ */
 $config = array(
-	'compiler' 	=> './lessphp/lessc.inc.php',
-	'less' 		=> './files/bootstrap.less',
-	'css' 		=> './files/style.css',
-	'debug' 	=> 0,
+
+	// Rendering mode
+	// String: demand, compile, both - can be overridden via GET var
+	'mode'			=> 'demand',
+
+	// LESS root file
+	// String: path/to/file.less, relative to css.php - can be overridden via GET var
+	'lessFile'		=> './files/styles.less',
+
+	// Stylesheet compiling dir
+	// String: path/to/dir/ to put the compiled CSS in, relative to css.php
+	'compilePath' 	=> './files/',
+
+	// Misc stuff
+	'minify'		=> 1, // boolean
+	'debug' 		=> 1, // boolean
 );
